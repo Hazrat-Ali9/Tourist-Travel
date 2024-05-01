a7427ebf867d1b2392e68aa69ec36a6e6c0f05f4
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-01 19:24:13 +0600

    Auth Provider :tada:

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,3 +1,4 @@
+// Auth Provider
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../Firebase/Firebase.config";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
